aed696f25961ef6cb886494bfd3547518416fb09
Josue Cueva <161115000+jjo64@users.noreply.github.com>
2026-02-20 14:08:31 +0100

    Desarrollo (#8)
    * fix vercel 1.1
    
    * Fix Railway
    
    * Intento 40mil de arreglar railway
    
    * Version estable de cinevault
    
    * Se actualizo el git ignore porque se implementaron skills al proyecto
    
    * Se implemento la verificacion por email para el registro y la eliminacion de usuarios en la BBDD que no esten verificados
    
    * Se agrego login con google, falta registrarlos en la BBDD
    
    * Actualizando librerias react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 import SearchResults from './pages/SearchResults'
