166c2b9eff631ea39185265ce114a76465cf77dd
jo <cuevajosuelm@gmail.com>
2026-03-03 19:52:31 +0100

    Se agregó rutas para la informacion y su controlador

--- backend/src/controllers/InformationController.ts
+++ backend/src/controllers/InformationController.ts
@@ -12,3 +12,28 @@ export const obtenerInformacionCombinada = async (req: Request, res: Response) =
     const data = await consultarTMDB(`person/${id}/combined_credits`)
     res.status(200).json(data)
 }
+
+//Simplemente buscar una persona para que con el click llevar a obtenerInformacion
+export const buscarPersona = async (req: Request, res: Response) => {
+    const query = req.query.query as string
+    const data = await consultarTMDB("search/person", { query })
+    res.status(200).json(data)
+}
+
+export const buscarTodo = async (req: Request, res: Response) => {
+    const query = req.query.query as string
+    const data = await consultarTMDB("search/multi", { query })
+    res.status(200).json(data)
+}
+
+export const buscarPelicula = async (req: Request, res: Response) => {
+    const query = req.query.query as string
+    const data = await consultarTMDB("search/movie", { query })
+    res.status(200).json(data)
+}
+
+export const buscarSerie = async (req: Request, res: Response) => {
+    const query = req.query.query as string
+    const data = await consultarTMDB("search/tv", { query })
+    res.status(200).json(data)
+}
--- backend/src/routes/information.routes.ts
+++ backend/src/routes/information.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
-import { middlewareAutenticacion } from "../middlewares/auth.middlewares.js"
 import { manejadorAsincrono } from "../middlewares/error.middlewares.js"
-import { obtenerInformacion, obtenerInformacionCombinada } from "../controllers/InformationController.js"
+import { obtenerInformacion, obtenerInformacionCombinada, buscarPersona, buscarTodo, buscarPelicula, buscarSerie } from "../controllers/InformationController.js"
 
 const router = Router()
 
-router.get("/:id", middlewareAutenticacion, manejadorAsincrono(obtenerInformacion)) // Información de una persona
-router.get("/:id/credits", middlewareAutenticacion, manejadorAsincrono(obtenerInformacionCombinada)) // Información de una persona pero con sus trabajos
-
+router.get("/:id/person", manejadorAsincrono(obtenerInformacion)) // Información de una persona
+router.get("/:id/person_credit", manejadorAsincrono(obtenerInformacionCombinada)) // Información de una persona pero con sus trabajos
+router.get('/person', manejadorAsincrono(buscarPersona))
+router.get('/todo', manejadorAsincrono(buscarTodo))
+router.get('/movie', manejadorAsincrono(buscarPelicula))
+router.get('/tv', manejadorAsincrono(buscarSerie))
 export default router
